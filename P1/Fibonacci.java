dc0a2c339170d9d7b2ca071af2b4c3d5a672aa76
KINGUEEN <773924560@qq.com>
2017-06-20 13:57:30 +0800

    斐波那契数列

--- P1/Fibonacci.java
+++ P1/Fibonacci.java
@@ -1,4 +1,5 @@
 package test.data;
+
 import java.util.*;
 import java.math.*;
 import java.io.*;
@@ -6,13 +7,18 @@ import java.math.BigInteger;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		BigInteger fib[] = new BigInteger[100];
-		fib[0] = fib[1] =BigInteger.ONE;
-		for (int i = 2; i < 100; i++) {
-			fib[i] = fib[i - 1].add(fib[i - 2]);
+		BigInteger fib[] = new BigInteger[100]; // 使用BigInteger定义数组
+		fib[0] = fib[1] = BigInteger.ONE; // 初始化数组值为1
+		System.out.println("请输入n的值");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		{
+			for (int i = 2; i < n; i++) {
+				fib[i] = fib[i - 1].add(fib[i - 2]);
+			}
 		}
-		for(int j=0;j<100;j++){
-			System.out.println(fib[j]);
+		for (int j = 0; j < n; j++) {
+			System.out.println(fib[j]); // 循环输出数组的值
 		}
 	}
 }
