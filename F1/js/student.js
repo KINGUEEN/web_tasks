dd56ab5ed520a3178f8c89a291dd0b8e04b159ad
KINGUEEN <773924560@qq.com>
2017-08-12 10:54:53 +0800

    修改

--- F1/js/student.js
+++ F1/js/student.js
@@ -1,5 +1,5 @@
 var storage = window.localStorage;
-function  student(name, sex,studentId, grade, major) {       
+function  student(name, sex,studentId, grade, major) {
   this.name = name || null;
   this.sex =  sex || null;
   this.studentId = studentId || null;
@@ -8,109 +8,129 @@ function  student(name, sex,studentId, grade, major) {
 }
 
 function addStu() {
-  var data = JSON.parse(storage.getItem("studentSys"));
-  if (data == null) {
-    data = new Array();
-  }
-  var stu = new student();
-  stu.name =document.getElementById("name").value;
-  stu.sex = document.getElementById("sex").value;
-  stu.studentId =document.getElementById("studentId").value;
-  stu.grade =  document.getElementById("grade").value;
-  stu.major =  document.getElementById("major").value;
-  if (Judge(stu)) {
-    data[stu.id] = stu;
-    storage.setItem("studentSys", JSON.stringify(data));
-    alert("success!");
-  }
+    var data = JSON.parse(storage.getItem("studentSys"));
+    if (data == null) {
+        data = new Array();
+    }
+    var stu = new student();
+    stu.name =$('#name').val().$.trim();
+    stu.sex = $('#sex').val().$.trim();
+    stu.studentId =d$('#studentId').val().$.trim();
+    stu.grade =  $('#grade').val().trim();
+    stu.major =  $('major').val().trim();
+    if (Judge(stu)) {
+        data[stu.id] = stu;
+        storage.setItem("studentSys", JSON.stringify(data));
+        alert("success!");
+    }
 }
 
 function Judge(student) {
-  if (student.name == "") {
-    alert("name can't be none !");
-    return false;
-  } else if (student.id == "") {
-    alert("id can't be none!");
-    return false;
-  } else {
-    return true;
-  }
+    if (student.name == "") {
+        alert("name can't be none !");
+        return false;
+    } else if (student.id == "") {
+        alert("id can't be none!");
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function searchStu() {
-  var id = document.getElementById("tid");
-  var stu = JSON.parse(storage.getItem("studentSys"))[id];
-  if (stu == null) {
-    alert("the student was not exist!");
-  } else {
-    document.getElementById("sName").innerHtml()=stu.name;
-    document.getElementById("sSex").innerHtml()=stu.sex;
-    document.getElementById("sId").innerHtml()=stu.studentId;
-    document.getElementById("sGrade").innerHtml()=stu.grade;
-    document.getElementById("sMajor").innerHtml()=stu.major;
-  }
+    var id = $("#tid").val().trim();
+    var stu = JSON.parse(storage.getItem("studentSys"))[id];
+    if (stu == null) {
+        alert("the student was not exist!");
+        $("#searchPut").html("");
+    } else {
+        $("#searchPut").html("<tr><td>" + stu.name + "</td><td>" + stu.sex + "</td><td>" + stu.id + "</td><td>" + stu.grade + "</td><td>" + stu.major + "</td></tr>");
+   }
 }
 
 function delStu() {
-  var id = document.getElementById("did").valueOf();
-  var data = JSON.parse(storage.getItem("studentSys"));
-  if (data[id] == null) {
-    alert("the student was not exist!");
-  } else {
-    data[id] = null;
-    storage.setItem("studentSys", JSON.stringify(data));
-    alert("delete successed!");
-  }
+    var id = $("#did").val().trim();
+    var data = JSON.parse(storage.getItem("studentSys"));
+    if (data[id] == null) {
+        alert("the student was not exist!");
+    } else {
+        data[id] = null;
+        storage.setItem("studentSys", JSON.stringify(data));
+        alert("delete Successed!");
+    }
 }
-
 function FormShow() {
-  var id = document.getElementById("tid").valueOf();
-  var data = JSON.parse(storage.getItem("studentSys"));
-  if (data[id] == null) {
-    alert("the student was not exist!");
-    $(".Form").hide();
-  } else {
-    $(".Form").fadeIn("slow");
-  }
+    var id = $("#tid").val().trim();
+    var data = JSON.parse(storage.getItem("studentSys"));
+    if (data[id] == null) {
+        alert("the student was not exist!");
+        $(".Form").hide();
+    } else {
+        $(".Form").fadeIn("slow");
+    }
 }
 
 function change() {
-  var id = $("#eId").val().trim();
-  var data = JSON.parse(storage.getItem("studentSys"));
-  if ($("#eName").val().trim() != "") {
-    data[id].name = document.getElementById("cName").value;
-    data[id].sex =  document.getElementById("cSex").value;
-    data[id].grade =  document.getElementById("cGrade").value;
-    data[id].major =  document.getElementById("cMajor").value;
-    storage.setItem("studentSys", JSON.stringify(data));
-    alert("change successed!");
-  } else {
-    alert("name can't be none !");
-  }
+    var id = $("#eId").val().trim();
+    var data = JSON.parse(storage.getItem("studentSys"));
+    if ($("#eName").val().trim() != "") {
+        data[id].name = $("cName").val().trim();
+        data[id].sex =  $("cSex").val().trim();
+        data[id].grade =  $("cGrade").val().trim();
+        data[id].major =  $("cMajor").val().trim();
+        storage.setItem("studentSys", JSON.stringify(data));
+        alert("change successed!");
+    } else {
+        alert("name can't be none !");
+    }
+}
+function showAllinfo(){
+    var data = JSON.parse(storage.getItem("stuSys"));
+    var outputer = $("#showAll");
+    if (data == null) {
+        outputer.empty();
+    } else {
+    outputer.empty();
+    for (var i in data) {
+        if (data[i] != null) {
+            outputer.append("<tr><td>" + data[i].name + "</td><td>" + data[i].sex + "</td><td>" + data[i].id + "</td><td>" + data[i].grade + "</td><td>" + data[i].subject + "</td></tr>");
+        }
+    }
+}
 }
 
-
 function addShow(){
-        $('.add').fadeIn('slow');
-        $('.search').hide();
-        $('.del').hide();
-        $('.change').hide();
-    }
-    function searchShow(){
-        $('.add').hide();
-        $('.search').fadeIn('slow');
-        $('.del').hide();
-        $('.change').hide();
-    }
-    function delShow(){
-        $('.add').hide();
-        $('.search').hide();
-        $('.del').fadeIn('slow');
-        $('.change').hide();
-    }
-    function changeShow(){
-        $('.add').hide();
-        $('.search').hide();
-        $('.del').hide();
-        $('.change').fadeIn('slow');
-    }
+    $('.add').fadeIn('slow');
+    $('.search').hide();
+    $('.del').hide();
+    $('.change').hide();
+    $('all').hide();
+}
+function searchShow(){
+    $('.add').hide();
+    $('.search').fadeIn('slow');
+    $('.del').hide();
+    $('.change').hide();
+    $('all').hide();
+}
+function delShow(){
+    $('.add').hide();
+    $('.search').hide();
+    $('.del').fadeIn('slow');
+    $('.change').hide();
+    $('all').hide();
+}
+function changeShow(){
+    $('.add').hide();
+    $('.search').hide();
+    $('.del').hide();
+    $('.change').fadeIn('slow');
+    $('all').hide();
+}
+function allShow(){
+    $('.add').hide();
+    $('.search').hide();
+    $('.del').hide();
+    $('.change').hide();
+    $('all').fadeIn('slow');
+}
--- F2/js/imgjs.js
+++ F2/js/imgjs.js
@@ -1,92 +1,87 @@
-var startX, stopX;
-var index = 1;
-var pre = 0;
-var len=$('.img-photo').length;;
+
 $(document).ready(function() {
-    var currentIndex = 0,
-    interval,
-    hasStarted = false,
-    t = 3000;
-    $('.img-photo:not(:first)').hide();
-    $('.imgPage:first').addClass('on');
-    $('.img-turn').hide();
+    var nowIndex = 0,
+    interval,startX,stopX,
+    isStarted = false,
+    t = 3000,
+    len=$('.img-photo').length;
+    function init(){
+        $('.img-photo:not(:first)').hide();
+        $('.imgPage:first').addClass('on');
+        $('.img-turn').hide();
+    }
     $('.img-photo, .img-pre, .img-next').hover(function() {
-    stop();
-    $('.img-turn').slideUp('500');
+        stop();
+        $('.img-turn').slideUp('500');
     }, function() {
     	$('.img-turn').slideDown('500');
         start();
     });
     $('.imgPage').hover(function(e) {
-    stop();
-    var preIndex = $(".imgPage").filter(".on").index();
-    currentIndex = $(this).index();
-    play(preIndex, currentIndex);
+        stop();
+        var preIndex = $(".imgPage").filter(".on").index();
+        nowIndex = $(this).index();
+        play(preIndex, nowIndex);
     }, function() {
-    start();
+        start();
     });
+
+    function imgPre(){
     $('.img-pre').unbind('click');
     $('.img-pre').bind('click', function() {
-    pre();
-    });
+        pre();
+    }
+    function imgNext(){
     $('.img-next').unbind('click');
     $('.img-next').bind('click', function() {
-    next();
-    });
+        next();
+    }
     function pre() {
-    var preIndex = currentIndex;
-    currentIndex = (--currentIndex + len) % len;
-    play(preIndex, currentIndex);
+    var preIndex = nowIndex;
+    nowIndex = (--nowIndex + len) % len;
+    play(preIndex, nowIndex);
     }
     function next() {
-    var preIndex = currentIndex;
-    currentIndex = ++currentIndex % len;
-    play(preIndex, currentIndex);
+    var preIndex = nowIndex;
+    nowIndex = ++nowIndex % len;
+    play(preIndex, nowIndex);
     }
-    function play(preIndex, currentIndex) {
-    $('.img-photo').eq(preIndex).fadeOut(500)
-    .parent().children().eq(currentIndex).fadeIn(1000);
-    $('.imgPage').removeClass('on');
-    $('.imgPage').eq(currentIndex).addClass('on');
+    function play(preIndex, nowIndex) {
+        $('.img-photo').eq(preIndex).fadeOut(500).parent().children().eq(nowIndex).fadeIn(1000);
+        $('.imgPage').removeClass('on');
+        $('.imgPage').eq(nowIndex).addClass('on');
     }
     function start() {
-        if(!hasStarted) {
-            hasStarted = true;
+        if(!isStarted) {
+            isStarted = true;
             interval = setInterval(next, t);
         }
     }
     function stop() {
         clearInterval(interval);
-        hasStarted = false;
+        isStarted = false;
     }
     start();
-    });
-$(".img").on("mousedown", function(event) {
-  event.preventDefault();
-  startX = event.pageX;
+    function toutch(){
+        $(".img").on("touchstart", function(event) {
+            event.preventDefault();
+            startX = event.originalEvent.changedTouches[0].clientX;
+        });
+
+        $(".img").on("touchend", function(event) {
+            event.preventDefault();
+            stopX = event.originalEvent.changedTouches[0].clientX;
+            eventEnd();
+        });
+        function eventEnd() {
+            if (startX - stopX > 20) {
+                $(".img-next").click();
+            }
+            else if (startX - stopX < -20) {
+                $(".img-pre").click();
+            }
+        }
+    }
 });
 
-$(".img").on("mouseup", function(event) {
-  event.preventDefault();
-  stopX = event.pageX;
-  eventEnd();
-});
 
-$(".img").on("touchstart", function(event) {
-  event.preventDefault();
-  startX = event.originalEvent.changedTouches[0].clientX;
-});
-
-$(".img").on("touchend", function(event) {
-  event.preventDefault();
-  stopX = event.originalEvent.changedTouches[0].clientX;
-  eventEnd();
-});
-
-function eventEnd() {
-  if (startX - stopX > 20) {
-    $(".img-next").click();
-  } else if (startX - stopX < -20) {
-    $(".img-pre").click();
-  }
-}
